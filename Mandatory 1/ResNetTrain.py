01d1ea08a4b81c39ae8d421e4b0f7380ed9e96be
Håkon Kornstad <haakongk@ifi.uio.no>
2025-03-08 11:48:09 +0100

    Working on Mandatory 1

--- Mandatory 1/ResNetTrain.py
+++ Mandatory 1/ResNetTrain.py
@@ -7,7 +7,7 @@ import numpy as np
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-def train_model(model, train_loader, val_loader, criterion, optimizer, file_path, num_epochs=10, early_stopping=4):
+def train_model(model, train_loader, val_loader, criterion, optimizer, file_path, num_epochs=10, early_stopping=3):
     model = model.to(device)
 
     train_accs, val_accs, map_scores, class_accs = [], [], [], []
@@ -16,27 +16,30 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, file_path
     epochs_no_improvement = 0
 
     for epoch in range(num_epochs):
-        model.train()  # Treningsmodus
+        model.train()  # putting the model into training mode
         running_loss, correct, total = 0.0, 0, 0
 
         for images, labels in train_loader:
-            images, labels = images.to(device), labels.to(device)
+            images, labels = images.to(device), labels.to(device) # getting the images from train_loader
 
-            optimizer.zero_grad()
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+            optimizer.zero_grad() # zeroing out the gradients
+            outputs = model(images) # predicting on the model
+            loss = criterion(outputs, labels) # finding the loss
+            loss.backward() # backprop
+            optimizer.step() # backprop
 
-            running_loss += loss.item()
-            _, preds = torch.max(outputs, 1)
-            correct += (preds == labels).sum().item()
-            total += labels.size(0)
+            running_loss += loss.item() # adding the loss to the running loss
+            _, preds = torch.max(outputs, 1) # deciding predictions
+            correct += (preds == labels).sum().item() # deciding which are correct
+            total += labels.size(0) # finding the total amount of classes
 
-        train_loss = running_loss / len(train_loader)
-        train_accuracy = correct / total
-        val_accuracy, val_loss, map_score, class_acc = evaluate_model(model, val_loader, criterion)
+        train_loss = running_loss / len(train_loader) # deciding the loss for this epoch
+        train_accuracy = correct / total # deciding the accuracy (total correct over total labels)
 
+        # end of ecoch evaluation
+        val_accuracy, val_loss, map_score, class_acc = evaluate_model(model, val_loader, criterion) 
+
+        # storing all the metrics for this epoch
         train_losses.append(train_loss)
         val_losses.append(val_loss)
         train_accs.append(train_accuracy)
@@ -44,72 +47,83 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, file_path
         map_scores.append(map_score)
         class_accs.append(class_acc)
 
+        # storing model if map_score has improved on previous ones
         if map_score > best_map_score:
             torch.save(model.state_dict(), file_path)
             print(f"Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}, "
                   f"Train Acc: {train_accuracy:.4f}, Val Acc: {val_accuracy:.4f}, mAP: {map_score:.4f} - Model Saved")
             best_map_score = map_score
-            epochs_no_improvement = 0
+            epochs_no_improvement = 0 # resetting the counter for early stopping
 
-        else:
+        else: # no improvement
             print(f"Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}, "
                   f"Train Acc: {train_accuracy:.4f}, Val Acc: {val_accuracy:.4f}, mAP: {map_score:.4f}")
             epochs_no_improvement += 1
 
-        if epochs_no_improvement > early_stopping:
-            print("Early stopping!")
+        # stopping if no improvement
+        if epochs_no_improvement == early_stopping:
+            print(f"Early stopping! No improvement in mAP on {epochs_no_improvement} epochs.")
             break
 
     return train_accs, val_accs, map_scores, class_accs, train_losses, val_losses
 
         
 def evaluate_model(model, dataloader, criterion):
-    model.eval()
+    model.eval() # setting to eval mode
     all_preds, all_labels, all_probs = [], [], []
     running_loss = 0.0
 
-    with torch.no_grad():
+    with torch.no_grad(): # not doing backprop
         for images, labels in dataloader:
             images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            loss = criterion(outputs, labels)
+            outputs = model(images) # predicting on the validation set
+            loss = criterion(outputs, labels) # getting the loss
 
             running_loss += loss.item()
 
-            probs = torch.softmax(outputs, dim=1)
-            _, preds = torch.max(outputs, 1)
+            probs = torch.softmax(outputs, dim=1) # getting the probabilities for each class with softmax
+            _, preds = torch.max(outputs, 1) # deciding predictions
 
             all_preds.append(preds.cpu())
             all_labels.append(labels.cpu())
             all_probs.append(probs.cpu())
 
     val_loss = running_loss / len(dataloader)
+
+    # concatenating all the batch predictions, labels and softmaxes
     all_preds = torch.cat(all_preds)
     all_labels = torch.cat(all_labels)
     all_probs = torch.cat(all_probs)
 
+    # using scikit's accuracy score
     accuracy = accuracy_score(all_labels, all_preds)
 
     ap_scores = []
     class_accs = []
     num_classes = all_probs.shape[1]
 
-    for i in range(all_probs.shape[1]):
-        binary_labels = (all_labels == i).float()
-        ap = average_precision_score(binary_labels.cpu().numpy(), all_probs[:, i].cpu().numpy())
+    for i in range(num_classes):
+        binary_labels = (all_labels == i).float().cpu().numpy()
+        class_probs = all_probs[:, i].cpu().numpy()
+
+        # getting the average precision score for this class
+        # average_precision_score() measures AUC for the Precision-Recall Curve
+        # lectured on March 7.
+        ap = average_precision_score(binary_labels, class_probs)
         ap_scores.append(ap)
 
-        class_correct = ((all_preds == i) & (all_labels == i)).sum().item()
+        class_correct = ((all_preds == i) & (all_labels == i)).sum().item() # correct scores for this class
         class_total = (all_labels == i).sum().item()
-        class_accuracy = class_correct / class_total if class_total > 0 else 0
+        class_accuracy = class_correct / class_total if class_total > 0 else 0 # avoiding division by zero
         class_accs.append(class_accuracy)
 
+    # getting the map_score, which is the mean of ap_scores for all classes
     map_score = sum(ap_scores) / len(ap_scores)
 
     return accuracy, val_loss, map_score, class_accs
 
 
-def predict(model, test_loader, file_path):
+def predict_softmax(model, test_loader, file_path):
     model.eval()
     softmaxes = []
 
@@ -131,7 +145,7 @@ def predict(model, test_loader, file_path):
 
 def compare_softmax(file_path, model, test_loader, tolerance=1e-5):
     df = pd.read_csv(file_path)
-    saved_labels = df["label"].values
+    #saved_labels = df["label"].values
     saved_softmax_scores = df.drop(columns=["label"]).values
 
     model.eval()
--- Mandatory 1/haakongk.py
+++ Mandatory 1/haakongk.py
@@ -12,11 +12,9 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
-import torchvision.models as models
+from torchvision.models import resnet34, ResNet34_Weights
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-torch.cuda.empty_cache()
-torch.cuda.memory_allocated(device=0)  # Se hvor mye minne PyTorch bruker
 
 def set_seed(seed):
 
@@ -44,7 +42,7 @@ transform = transforms.Compose([
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
-# data augmentation parameters
+# data augmentation
 augm_transform = transforms.Compose([
     transforms.RandomResizedCrop(150, scale=(0.8, 1.0)),
     transforms.RandomHorizontalFlip(p=0.5),
@@ -66,33 +64,48 @@ train_loader_augm = DataLoader(train_dataset_augm, batch_size=32, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
-# #### Setting up the training ######
-print("\nStarting training of model: ResNet34, CrossEntropyLoss, Adam, lr: 0.001, basic transforms")
+
+##### Training three models #####
+
+##### Model 1 #####
+print("\nStarting training of model 1/3: ResNet34, CrossEntropyLoss, Adam, lr: 0.001, basic transforms")
 class_names = preprocessor.get_class_names()
 criterion = nn.CrossEntropyLoss()
 model = ResNet(img_channels=3, num_layers=34, num_classes=len(class_names))
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 file_path = '/mnt/e/ml_projects/IN3310/2025/tut_data/oblig1/resnet1.pth'
-train_acc1, val_acc1, map_scores1, class_accs1, train_losses1, val_losses1 = train_model(model, train_loader, val_loader, criterion, optimizer, file_path, num_epochs=20)
+train_accs1, val_acc1, map_scores1, class_accs1, train_losses1, val_losses1 = train_model(model, train_loader, val_loader, criterion, optimizer, file_path, num_epochs=20)
 
-# print("\nStarting training of model: ResNet34, CrossEntropyLoss, Adam, lr: 0.001, augmented transforms")
-# criterion = nn.CrossEntropyLoss()
-# model = ResNet(img_channels=3, num_layers=34, num_classes=len(class_names))
-# optimizer = optim.Adam(model.parameters(), lr=0.001)
-# file_path = '/mnt/e/ml_projects/IN3310/2025/tut_data/oblig1/resnet2.pth'
-# train_acc2, val_acc2, map_scores2, class_accs2, train_losses2, val_losses2 = train_model(model, train_loader_augm, val_loader, criterion, optimizer, file_path, num_epochs=15)
+##### Model 2 #####
+print("\nStarting training of model 2/3: ResNet34, CrossEntropyLoss, Adam, lr: 0.001, augmented transforms")
+criterion = nn.CrossEntropyLoss()
+model = ResNet(img_channels=3, num_layers=18, num_classes=len(class_names))
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+file_path = '/mnt/e/ml_projects/IN3310/2025/tut_data/oblig1/resnet2.pth'
+train_acc2, val_acc2, map_scores2, class_accs2, train_losses2, val_losses2 = train_model(model, train_loader_augm, val_loader, criterion, optimizer, file_path, num_epochs=20)
 
-# print("\nStarting training of model: ResNet34, CrossEntropyLoss, SGD, lr: 0.005, basic transforms")
-# criterion = nn.CrossEntropyLoss()
-# model = ResNet(img_channels=3, num_layers=34, num_classes=len(class_names))
-# optimizer = optim.SGD(model.parameters(), lr=0.005)
-# file_path = '/mnt/e/ml_projects/IN3310/2025/tut_data/oblig1/resnet3.pth'
-# train_acc3, val_acc3, map_scores3, class_accs3, train_losses3, val_losses3 = train_model(model, train_loader, val_loader, criterion, optimizer, file_path, num_epochs=15)
-# print("Finished training.)
+##### Model 3 #####
+print("\nStarting training of model 3/3: ResNet34, CrossEntropyLoss, SGD, lr: 0.005, basic transforms")
+criterion = nn.CrossEntropyLoss()
+model = ResNet(img_channels=3, num_layers=34, num_classes=len(class_names))
+optimizer = optim.SGD(model.parameters(), lr=0.005)
+file_path = '/mnt/e/ml_projects/IN3310/2025/tut_data/oblig1/resnet3.pth'
+train_acc3, val_acc3, map_scores3, class_accs3, train_losses3, val_losses3 = train_model(model, train_loader, val_loader, criterion, optimizer, file_path, num_epochs=20)
+
+# deciding on best model
+class_accs = [class_accs1, class_accs2, class_accs3]
+map_scores = [map_scores1, map_scores2, map_scores3]
+train_losses = [train_losses1, train_losses2, train_losses3]
+val_losses = [val_losses1, val_losses2, val_losses3]
+
+best_map_scores = [np.max(map_scores1), np.max(map_scores2), np.max(map_scores3)]
+best_model = np.argmax(best_map_scores)
+
+print(f"Finished training. The best model was number {best_model + 1}, with mAP-score {np.max(best_map_scores):.4f}")
 
 ##### Plotting ######
-plot_map_per_class(class_names, class_accs1, map_scores1, BASE_PATH, 'plot_map_scores1.png')
-plot_train_val_loss(train_losses1, val_losses1, BASE_PATH, 'plot_train_val_loss1.png')
+plot_map_per_class(class_names, class_accs[best_model], map_scores[best_model], BASE_PATH, f'plot_map_scores{best_model + 1}.png')
+plot_train_val_loss(train_losses[best_model], val_losses[best_model], BASE_PATH, f'plot_train_val_loss{best_model + 1}.png')
 print('Plots saved.')
 
 ##### Predicting on model and saving softmax results #####
@@ -100,16 +113,15 @@ print("Finished training. Predicting on best model")
 
 model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
 model = model.to(device)
-
-predict(model, test_loader, SOFTMAX_CSV)
+predict_softmax(model, test_loader, SOFTMAX_CSV)
 evaluate_on_test_set(model, test_loader, criterion)
 compare_softmax(SOFTMAX_CSV, model, test_loader)
 
 ###### Using Pretrained Model ######
 print("Loading pretrained model")
-model = models.resnet34(pretrained=True)
+model = resnet34(weigths = ResNet34_Weights.DEFAULT)
 
-# changing the output layer
+# changing the output layer to fit our classes
 num_classes = len(class_names)
 model.fc = nn.Linear(model.fc.in_features, num_classes)
 model = model.to(device)
@@ -121,8 +133,7 @@ optimizer = optim.Adam(model.parameters(), lr=0.0001)
 # training
 print("Starting training on pretrained model: ResNet34, CrossEntropyLoss, Adam, lr: 0.0001, basic transforms")
 train_accs0, val_accs0, map_scores0, class_accs0, train_losses0, val_losses0 = train_model(
-    model, train_loader, val_loader, criterion, optimizer, "pretrained_resnet34.pth", num_epochs=10, early_stopping=5
-)
+    model, train_loader, val_loader, criterion, optimizer, "pretrained_resnet34.pth", num_epochs=10)
 
 # plotting
 plot_map_per_class(class_names, class_accs0, map_scores0, BASE_PATH, 'plot_map_scores0.png')
@@ -130,6 +141,6 @@ plot_train_val_loss(train_losses0, val_losses0, BASE_PATH, 'plot_train_val_loss0
 print('Plots saved.')
 
 print("Finished training. Predicting on model")
-predict(model, test_loader, SOFTMAX_CSV_PRE)
+predict_softmax(model, test_loader, SOFTMAX_CSV_PRE)
 evaluate_on_test_set(model, test_loader, criterion)
 compare_softmax(SOFTMAX_CSV_PRE, model, test_loader)
